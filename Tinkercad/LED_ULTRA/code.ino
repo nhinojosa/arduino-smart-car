17423291048807178c6db1cef5bcd8dc62bc5311
nhinojosa <nhinojosa@southtexascollege.edu>
2022-07-21 08:20:19 -0500

    Create code.ino

---
+++ Tinkercad/LED_ULTRA/code.ino
@@ -0,0 +1,43 @@
+// C++ code
+//
+const int triggerPin = 12;
+const int echoPin = 13;
+const int LED = 9;
+void setup()
+{
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+  Serial.print("Hello, I am WALL-E!");
+  Serial.println();
+}
+
+void loop()
+{
+    double duration, cm, in;
+	digitalWrite(triggerPin, LOW);
+  	delayMicroseconds(2);
+  	digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+  	digitalWrite(triggerPin, LOW);
+  	duration = pulseIn(echoPin, HIGH);
+  	cm = duration / 29 / 2;
+  	in = duration / 74 / 2;
+    if( in < 10 )
+    {
+        digitalWrite(LED,HIGH);   
+    }
+    else 
+    {
+      digitalWrite(LED,LOW);
+    }
+  
+    Serial.print( "Distance: ");
+    Serial.print(in);
+ 	Serial.print(" IN/ ");
+  	Serial.print(cm);
+ 	Serial.println(" CM");
+  	delay(100);
+    
+}
